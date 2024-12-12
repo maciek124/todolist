bb64526a77dea81ea7e319f8943820e1a93e1d06
Maciek <jasek1264@wp.pl>
2024-12-12 12:53:16 +0100

    :c

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
-  trailingSlash: true, 
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/todolist',
+  assetPrefix: '/todolist',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
